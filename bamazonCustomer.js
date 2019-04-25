cb6a17a09a73fa868c653b51358d78b86c9475ca
jzubak <jzubak81@gmail.com>
2019-04-24 22:14:49 -0400

    wednesday 1015

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,22 +12,125 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-
+    // connection.query(list)
     inquirer.prompt([
         {
-            type: "input",
-            name: "bamazon",
-            message: "What is the ID Number of the item you want to buy?"
-        }]).then(function(itemquery) {
-            console.log(itemquery)
+            type: "list",
+            name: "bamazonsearch",
+            message: "How would you like to search for an item?",
+            choices: ["Item ID", "Product Name", "Product Department", "Price Range"]
+
+
+        }]).then(function (choices) {
+            if (choices.bamazonsearch === "Item ID") {
+                byItemId()
+            };
+            // if (choices.bamazonsearch === "Product Name") {
+            //     byProduct()
+            // }
+            // if (choices.bamazonsearch === "Product Department") {
+            //     byDepartment()
+            // }
+            // if (choices.bamazonsearch === "Price Range") {
+            //     byPrice()
+            // }
+
+        });
+
+
+    function byItemId(itemsearch) {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "bamazon",
+                message: "What is the ID Number of the item you want to buy?"
+            }]).then(function (itemquery) {
                 connection.query("select * from products where item_id = ?", itemquery.bamazon, function (err, res) {
+                    var item = res[0];
                     if (err) throw err;
-                    for (var i = 0; i < res.length; i++) {
-                        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+                    // for (var i = 0; i < res.length; i++) {
+                    console.log("You are searching for item #" + item.item_id + "\n name: " + item.product_name + "\n department: " + item.department_name + "\n unit price: " + item.price);
+                    if (item.stock_quantity != 0) {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                name: "buy",
+                                message: `How many ${item.product_name} do you want to buy?`
+                            }
+                        ]).then(function (res) {
+                            console.log(item)
+                            purchase(res, item)
+                        });
+                        // //     function (purchase){
+                        //         console.log(item)
+                        //     connection.query("UPDATE products SET ? WHERE ?",
+                        //     [
+                        //         {
+                        //             stock_quantity: `stock_quantity -${purchase.buy}`
+                        //         },
+                        //         {
+                        //             item_id: `${item.item_id}`
+                        //         }
+                        //     ],
+                        //     function (err, res) {
+                        //         console.log(res.affectedRows + " products updated!\n");
+                        //         // Call deleteProduct AFTER the UPDATE completes
+                        //         // deleteProduct();
                     }
-                    connection.end();
+                    // }
+                    // );
+                    // }
+                    // else console.log("I'm sorry this item is currently out of stock")
+                    // outofstock();
+                    // };
                 })
             })
-       
-    });
+
+        function purchase(res, item) {
+            console.log("\nUpdating the item you selected to purchase\n");
+            connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock: `stock_quantity -${purchase.buy}`
+                    },
+                    {
+                        item_id: `${item.item_id}`
+                    }
+                ],
+                function (err, res) {
+                    console.log(res + " products updated!\n");
+                    // Call deleteProduct AFTER the UPDATE completes
+                    // deleteProduct();
+                
+                connection.end();
+            });            
+        }
+
+function deleteProduct() {
+    console.log("Deleting all strawberry icecream...\n");
+    connection.query(
+        "DELETE FROM products WHERE ?",
+        {
+            flavor: "strawberry"
+        },
+        function (err, res) {
+            console.log(res.affectedRows + " products deleted!\n");
+            // Call readProducts AFTER the DELETE completes
+            readProducts();
+        }
+    );
+}
+
+function outofstock() {
+    inquirer.prompt([{
+        type: "list",
+        name: "outofstock",
+        message: "We're sorry this item is currently out of stock. Would you like to search for something else?",
+        choices: ["Yes please!", "No thank you I really just wanted that item"]
+    }])
+    connection.end();
+}
+    }
+});
 
--- bamazonalternate1.js
+++ bamazonalternate1.js
@@ -36,74 +36,147 @@ connection.connect(function (err) {
             }
         })
 
-    // A query which returns all data for songs sung by a specific artist
-    function artist() {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "artist_name",
-                message: "which artist do you want to search for?"
-            }]).then(function(artistquery) {
-                connection.query("select * from songs where artist = ?", artistquery.artist_name, function (err, res) {
-                    if (err) throw err;
-                    for (var i = 0; i < res.length; i++) {
-                        console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
-                    }
-                    connection.end();
-                })
-            })
-        }
-    //A query which returns all artists who appear within the top 5000 more than once
-    function duplicates() {
-        connection.query("select position, artist, count(*) from songs group by artist having count(*)>1", function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log(res[i].position + " | " + res[i].artist + " | " + res[i].count);
-            }
-            connection.end();
-        })
-    }
-
-    //A query which returns all data contained within a specific range
-    function range() {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "range1",
-                message: "what is the start of the range do you want to see songs in?"
-            },
-            {
-                type: "input",
-                name: "range2",
-                message: "what is the end of the range do you want to see songs in?"
-            }])
-            .then(function(rangeSearch) {
-        connection.query("select distinct * from songs where position between ? and ?;", [rangeSearch.range1, rangeSearch.range2], function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
-            }
-            connection.end();
-        })
-    })
-    }
-
-    //A query which searches for a specific song in the top 5000 and returns the data for it
-    function specificSong() {
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "song_name",
-                message: "which song do you want to search for?"
-            }]).then(function(songquery) {
-        connection.query("select distinct * from songs where title =?", songquery.song_name, function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
-            }
-            connection.end();
-        })
-    })
-    }
 });
 
+// A query which returns all data for songs sung by a specific artist
+function artist() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "artist_name",
+            message: "which artist do you want to search for?"
+        }]).then(function (artistquery) {
+            connection.query("select * from songs where artist = ?", artistquery.artist_name, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
+                }
+                connection.end();
+            })
+        })
+}
+//A query which returns all artists who appear within the top 5000 more than once
+function duplicates() {
+    connection.query("select position, artist, count(*) from songs group by artist having count(*)>1", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].position + " | " + res[i].artist + " | " + res[i].count);
+        }
+    connection.end();
+});
+
+//A query which returns all data contained within a specific range
+function range() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "range1",
+            message: "what is the start of the range do you want to see songs in?"
+        },
+        {
+            type: "input",
+            name: "range2",
+            message: "what is the end of the range do you want to see songs in?"
+        }])
+        .then(function (rangeSearch) {
+            connection.query("select distinct * from songs where position between ? and ?;", [rangeSearch.range1, rangeSearch.range2], function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
+                }
+                connection.end();
+            })
+        })
+};
+
+//A query which searches for a specific song in the top 5000 and returns the data for it
+function specificSong() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "song_name",
+            message: "which song do you want to search for?"
+        }]).then(function (songquery) {
+            connection.query("select distinct * from songs where title =?", songquery.song_name, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    console.log(res[i].position + " | " + res[i].artist + " | " + res[i].title + " | " + res[i].year + " | " + res[i].popularity_world + " | " + res[i].popularity_USA + " | " + res[i].popularity_UK + " | " + res[i].popularity_Europe + " | " + res[i].popularity_rest);
+                }
+                connection.end();
+            })
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+        function bidAuction() {
+            // query the database for all items being auctioned
+            connection.query("SELECT * FROM auctions", function(err, results) {
+              if (err) throw err;
+              // once you have the items, prompt the user for which they'd like to bid on
+              inquirer
+                .prompt([
+                  {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                      var choiceArray = [];
+                      for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].item_name);
+                      }
+                      return choiceArray;
+                    },
+                    message: "What auction would you like to place a bid in?"
+                  },
+                  {
+                    name: "bid",
+                    type: "input",
+                    message: "How much would you like to bid?"
+                  }
+                ])
+                .then(function(answer) {
+                  // get the information of the chosen item
+                  var chosenItem;
+                  for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_name === answer.choice) {
+                      chosenItem = results[i];
+                    }
+                  }
+          
+                  // determine if bid was high enough
+                  if (chosenItem.highest_bid < parseInt(answer.bid)) {
+                    // bid was high enough, so update db, let the user know, and start over
+                    connection.query(
+                      "UPDATE auctions SET ? WHERE ?",
+                      [
+                        {
+                          highest_bid: answer.bid
+                        },
+                        {
+                          id: chosenItem.id
+                        }
+                      ],
+                      function(error) {
+                        if (error) throw err;
+                        console.log("Bid placed successfully!");
+                        start();
+                      }
+                    );
+                  }
+                  else {
+                    // bid wasn't high enough, so apologize and start over
+                    console.log("Your bid was too low. Try again...");
+                    start();
+                  }
+                });
+            });
+          }
+    
